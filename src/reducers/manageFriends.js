b7fd2b636f5958c342bc4b0f0361b721f6c897f8
mattenbar <matthw.enbar@gmail.com>
2020-08-15 11:30:15 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      let newFriends =  {...state.friends}
+
+      return {friends: newFriends}
+    default:
+      return state;
+  }
 }
