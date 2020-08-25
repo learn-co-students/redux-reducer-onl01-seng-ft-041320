2b463fe0eecbf29d78a8705b4f07a69695cd9b38
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-25 01:11:18 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,34 @@
-export function manageFriends(state, action){
-}
+export function manageFriends(state = {friends: []}, action){ 
+    console.log(state.friends)
+    
+   switch (action.type){
+       case 'ADD_FRIEND':
+           return { ...state,
+            friends: [
+               ...state.friends,
+               action.friend
+            ]}
+           case 'REMOVE_FRIEND':
+               const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+            return (
+                {...state,
+                    friends: [
+                        ...state.friends.slice(0, removalIndex),
+                        ...state.friends.slice(removalIndex +1)
+                    ]
+                }
+            )
+            default:
+                return state;
+
+   }
+};
+
+// action = {
+//     type: "ADD_FRIEND",
+//     friend: {
+//       name: "Chrome Boi"
+//       homewtown: "NYC",
+//       id: 1
+//     }
+//   }
