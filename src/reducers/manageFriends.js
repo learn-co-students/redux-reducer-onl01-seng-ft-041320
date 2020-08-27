0505fb27aab7d0d0332cee0ff4b90dd2aa46ae6c
Dhrubot <dhrubo.dh@gmail.com>
2020-08-27 07:56:29 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,27 @@
-export function manageFriends(state, action){
+
+// const state = {
+//     numberOfPresents: 0
+// }
+
+// const action = {
+//     type: 'INCREASE'
+// }
+// export function manageFriends(state, action){
+//     switch (action.type) {
+//         case 'INCREASE':
+//         return {numberOfPresents: state.numberOfPresents + 1 }
+//     default:
+//         return state
+//     }
+// 
+// }
+export function manageFriends(state={friends: []}, action) {
+    switch (action.type){
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            return {friends: state.friends.filter(friend => friend.id !== action.id)}
+        default:
+            return state
+    }
 }
