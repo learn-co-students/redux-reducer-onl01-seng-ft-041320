ad1295db5ffbd902c06ade594c323e4505f1ef15
dannypadron09 <danny.padron09@gmail.com>
2020-08-28 19:22:24 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
+//a function called `managePresents` that takes in the previous state and an action as its argument.
 export function managePresents(state, action){
+
+    switch(action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return state 
+    }
+
 }
